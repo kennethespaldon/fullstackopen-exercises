ce21d7a8ff0c43356f958ff428e13d7f938556d0
Kenneth Espaldon <kenespaldon@gmail.com>
2025-11-29 08:53:54 -0800

    initial commit

---
+++ part4/controllers/blogs.js
@@ -0,0 +1,18 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then(result => {
+    response.status(204).json(result);
+  });
+});
+
+module.exports = blogsRouter;
